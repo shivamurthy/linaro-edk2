ccf3b1489e744aa0d1b0d0a2108551e74e9d3492
Shivamurthy Shastri <shivamurthy.shastri@linaro.org>
2015-12-08 07:49:44 +0530

    EmbeddedPkg/AndroidFastbootTransportTcpDxe: Resolved compiler and build warnings
    Resolved below errors:
    1. Variable 'gEfiHostnameVariableGuid' used, but not defined GUID.
    2. File 'Hostname.h' is included, but it is not available in source.
    
    Signed-off-by: Shivamurthy Shastri <shivamurthy.shastri@linaro.org>

---
+++ EmbeddedPkg/Include/Guid/Hostname.h
@@ -0,0 +1,20 @@
+/** @file
+*
+*  Copyright (c) 2015, Linaro Limited. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#ifndef __HOSTNAME_H__
+#define __HOSTNAME_H__
+
+extern EFI_GUID gEfiHostnameVariableGuid;
+
+#endif /* __HOSTNAME_H__ */
